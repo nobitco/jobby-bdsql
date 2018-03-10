59160a6c475f303e423fd0c5675ca2d457277279
jesmarquez <jesmqz@gmail.com>
2018-03-10 11:18:51 -0500

    Confirma inicializacion base de datos

--- setup.js
+++ setup.js
@@ -2,8 +2,24 @@
 
 const db = require('./')
 const debug = require('debug')('platziverse:db')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
+
+const prompt = inquirer.createPromptModule()
 
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This destroy database! Are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened!')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'jobby',
     username: process.env.DB_USER || 'admjobby',
@@ -20,7 +36,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[fatal error]')}  ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
