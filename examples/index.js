16e5872eafff77adbcdf588fdad83b4a5716347b
jesmarquez <jesmqz@gmail.com>
2018-03-23 14:59:20 -0500

    Refactorizacion database seeding
     Se usa fakerjs para generar datos masivos.

--- examples/index.js
+++ examples/index.js
@@ -1,4 +1,6 @@
 const db = require('../')
+const faker = require('faker/locale/es')
+const TOTAL_USERS = 20
 
 async function run () {
   const config = {
@@ -11,17 +13,26 @@ async function run () {
 
   const { User } = await db(config).catch(handleFatalError)
 
-  const user = await User.createOrUpdate({
-    username: 'ccolascoaga',
-    password: '54321'
-  }).catch(handleFatalError)
-
-  console.log('--usuario--')
-  console.log(user)
+  for (var i = 0; i < TOTAL_USERS; i++) {
+    const user = await User.createOrUpdate({
+      username: faker.internet.userName(),
+      password: faker.internet.password()
+    }).catch(handleFatalError)
+  }
 
   const users = await User.findAll().catch(handleFatalError)
   console.log('--usuarios--')
   console.log(users)
+
+  /*
+  const borrado = await User.deleteByUsername('juana.hanz').catch(handleFatalError)
+  console.log('--usuario borrado--')
+  if (borrado) {
+    console.log('Deleted ok!')
+  } else {
+    console.log('Deleted fail!')
+  }
+  */
 }
 
 function handleFatalError (err) {
